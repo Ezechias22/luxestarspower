fc5feaf9689262b9a10ead2022a61de1c6e90810
Ezechias22 <toussaintezechias16@gmail.com>
2025-12-21 21:08:51 -0300

    Fix shop products query - use direct DB query instead of getBySellerPaginated

--- app/Controllers/ShopController.php
+++ app/Controllers/ShopController.php
@@ -41,7 +41,11 @@ class ShopController {
         }
         
         // Récupère les produits du vendeur
-        $products = $this->productRepo->getBySellerPaginated($seller['id'], 1, 12);
+        // Récupère les produits du vendeur
+$products = $this->db->fetchAll(
+    "SELECT * FROM products WHERE seller_id = ? AND is_active = 1 ORDER BY created_at DESC LIMIT 12",
+    [$seller['id']]
+);
         
         // Statistiques de la boutique
         $stats = $this->getShopStats($seller['id']);
