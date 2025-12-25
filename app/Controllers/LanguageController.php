264cf4fbf793184642242134d746684419b28101
Ezechias22 <toussaintezechias16@gmail.com>
2025-12-25 19:47:09 -0300

    Fix language switcher to accept params array

--- app/Controllers/LanguageController.php
+++ app/Controllers/LanguageController.php
@@ -4,14 +4,15 @@ namespace App\Controllers;
 use App\I18n;
 
 class LanguageController {
-    public function switch($locale) {
+    public function switch($params) {
+        $locale = $params['locale'] ?? 'fr';
         $availableLocales = ['fr', 'en', 'es', 'de', 'it'];
-        
+
         if (in_array($locale, $availableLocales)) {
             $_SESSION['locale'] = $locale;
             I18n::setLocale($locale);
         }
-        
+
         // Redirect back
         $referer = $_SERVER['HTTP_REFERER'] ?? '/';
         header('Location: ' . $referer);
